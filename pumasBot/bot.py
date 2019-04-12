b7767a1f52ea945e6db010ede3e8f275ec207f7e
PumaConcolor <puma.9789@gmail.com>
2019-04-12 11:24:55 +0200

    fix some stuff. I think

--- pumasBot/bot.py
+++ pumasBot/bot.py
@@ -1,5 +1,4 @@
 import telebot
-import json
 
 TOKEN = '860774372:AAE3yanl0Rl89rXaM34zaSvxbg6d3WMRNWA'
 
@@ -12,16 +11,17 @@ sbabba_photo = '/spam ' + u'\U0001F621'
 @bot.message_handler(commands=['start', 'help'])
 def welcome(message):
     bot.reply_to(message, welcome_message)
+    pass
 
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    if(message.from_user.username == 'BarbaraMartinengo'):
+def handle_text(message):
+    if(message.chat.username == 'BarbaraMartinengo'):
         bot.reply_to(message, sbabba_msg)
     pass
 
 @bot.message_handler(content_types=['photo'])
-def echo_photo(message):
-    if(message.from_user.username == 'BarbaraMartinengo'):
+def handle_photo(message):
+    if(message.chat.username == 'BarbaraMartinengo'):
         bot.reply_to(message, sbabba_photo)
     pass
 
