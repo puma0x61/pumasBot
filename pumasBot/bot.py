2b5081d7270749aa5673fd777475cd19947959cf
PumaConcolor <puma.9789@gmail.com>
2019-05-07 11:43:16 +0200

    can\'t remember

--- pumasBot/bot.py
+++ pumasBot/bot.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import telebot
 
 TOKEN = '860774372:AAE3yanl0Rl89rXaM34zaSvxbg6d3WMRNWA'
@@ -5,6 +7,8 @@ TOKEN = '860774372:AAE3yanl0Rl89rXaM34zaSvxbg6d3WMRNWA'
 bot = telebot.TeleBot(TOKEN)
 
 welcome_message = 'Welcome!\nThis bot was only built for his uselessnes, DO NOT try to use it with a purpose'
+help_message = 'No help for you'
+weather_message = 'Look for yourself.\nyr.no\nmeteoblue.com'
 sbabba_msg = 'blblblblblblblblblb ' + u'\U0001F61D'
 sbabba_photo = '/spam ' + u'\U0001F621'
 
@@ -13,10 +17,16 @@ def welcome(message):
     bot.reply_to(message, welcome_message)
     pass
 
+@bot.message_handler(commands=['weather'])
+def weather(message):
+    bot.reply_to(message, weather_message)
+    pass
+
 @bot.message_handler(func=lambda m: True)
 def handle_text(message):
     if(message.chat.username == 'BarbaraMartinengo'):
         bot.reply_to(message, sbabba_msg)
+    print(message)
     pass
 
 @bot.message_handler(content_types=['photo'])
@@ -25,5 +35,5 @@ def handle_photo(message):
         bot.reply_to(message, sbabba_photo)
     pass
 
-bot.polling()
+bot.polling(none_stop=False, interval=0, timeout=20)
 
