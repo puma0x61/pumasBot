605fce9c111bb4845b34284a237bcfe7a12eb1ac
PumaConcolor <puma.9789@gmail.com>
2022-04-06 18:38:54 +0200

    Add weather with openweather API

--- pumasBot/core/constants.py
+++ pumasBot/core/constants.py
@@ -3,3 +3,9 @@ WELCOME_MESSAGE = 'Welcome!\nThis bot was only built for his uselessnes, DO NOT 
 HELP_MESSAGE = 'No help for you'
 
 WEATHER_MESSAGE = 'Look for yourself.\nyr.no\nmeteoblue.com'
+
+LIMIT = 1
+
+EXCLUDE = 'minutely,hourly,alerts'
+
+WEATHER_ERROR = 'Couldn\'t get weather data, try again in a minute'
